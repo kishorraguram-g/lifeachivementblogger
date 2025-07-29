496bbdd11c1f918bf81cce4104c4e223e0352bf0
KISHORRAGURAM <kishorraguram.g2023cse@sece.ac.in>
2025-07-29 18:36:59 +0530

    updated login logic and data fetching

--- app/profile/page.js
+++
@@ -1,9 +0,0 @@
-import ProfilePage from "@/components/ProfilePage";
-
-export default function Profile() {
-  return (
-    <div className="container mx-auto p-4">
-      <ProfilePage />
-    </div>
-  );
-}
